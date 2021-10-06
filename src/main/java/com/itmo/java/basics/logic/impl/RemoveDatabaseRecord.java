730d3792fe6765cd4bf92d1e71fe554f60fb9ff8
leandoerbore <rusa8612331@gmail.com>
2021-10-06 19:38:29 +0300

    fixValueMax

--- src/main/java/com/itmo/java/basics/logic/impl/RemoveDatabaseRecord.java
+++ src/main/java/com/itmo/java/basics/logic/impl/RemoveDatabaseRecord.java
@@ -9,9 +9,6 @@ public class RemoveDatabaseRecord implements WritableDatabaseRecord
 
     private final byte[] _key;
 
-    //public static WritableDatabaseRecord create(byte[] objectKey) { return new RemoveDatabaseRecord(objectKey); }
-
-    //private RemoveDatabaseRecord(byte[] objectKey) { _key = objectKey; }
     public RemoveDatabaseRecord(byte[] objectKey) { _key = objectKey; }
 
     @Override
